65312504c8218b9586be2360181b9d2b9738faa1
Andrew Ying <hi@andrewying.com>
2019-04-07 19:26:14 +0100

    Use React.js

--- Pi3/src/js/components/Console.js
+++ Pi3/src/js/components/Console.js
@@ -4,7 +4,20 @@ class Console extends React.Component {
     render() {
         return (
             <div>
-                <video className="console" />
+                <p>
+                    <strong>Status:</strong> Connecting...
+                </p>
+                <div className="console__container">
+                    <video className="console" />
+                    <div className="console__overlay">
+                        <h2>Connecting to Remote Machine</h2>
+                        <h2 className="console__loading_container">
+                            <span className="console__loading">.</span>&nbsp;
+                            <span className="console__loading">.</span>&nbsp;
+                            <span className="console__loading">.</span>&nbsp;
+                        </h2>
+                    </div>
+                </div>
             </div>
         )
     }
---
+++ Pi3/src/js/reducers/index.js
@@ -0,0 +1,10 @@
+let initialState = {
+    iso: undefined
+};
+
+export default function app(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
