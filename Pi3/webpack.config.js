da129ebf614c0dcb32e15198f763944fb74c8cd5
Andrew Ying <hi@andrewying.com>
2019-04-07 15:53:38 +0100

    Add webpack for asset pipeline

---
+++ Pi3/webpack.config.js
@@ -0,0 +1,97 @@
+const path = require("path");
+const webpack = require("webpack");
+const ManifestPlugin = require("webpack-manifest-plugin");
+
+const ExtractCssChunks = require("extract-css-chunks-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+const production = process.env.NODE_ENV === "production";
+
+let config = {
+    mode: production ? "production" : "development",
+    entry: {
+        app: path.join(__dirname, "src", "index.js")
+    },
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: production ? "js/[name]-[chunkhash].js" : "js/[name].js",
+        chunkFilename: production ? "js/[name]-[chunkhash].js" : "js/[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: "babel-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: 'images/[name].[ext]',
+                    },
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: ExtractCssChunks.loader,
+                        options: {
+                            hot: !production
+                        }
+                    },
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new ExtractCssChunks(
+            {
+                filename: production ? "css/[name]-[chunkhash].css" : "css/[name].css",
+                chunkfilename: production ? "css/[name]-[id].css" : "css/[name].css"
+            }
+        ),
+        new ManifestPlugin({
+            writeToFileEmit: true,
+        }),
+    ],
+    optimization: {
+        minimize: production,
+        splitChunks: {
+            cacheGroups: {
+                default: false,
+                vendors: {
+                    test: /[\\/]node_modules[\\/].*js/,
+                    name: "vendor",
+                    chunks: "initial",
+                    enforce: true
+                }
+            }
+        }
+    }
+};
+
+if (production) {
+    config.plugins.push(
+        new webpack.DefinePlugin({
+            "process.env": { NODE_ENV: JSON.stringify("production") }
+        })
+    );
+    config.minimizer.push(new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+    }));
+} else {
+    config.plugins.push(
+        new webpack.NamedModulesPlugin(),
+    );
+    config.devtool = "source-map";
+}
+
+module.exports = config;
