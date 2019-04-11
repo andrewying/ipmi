efa4780f2d69296fa30860f286f54663de84fd65
Andrew Ying <hi@andrewying.com>
2019-04-11 10:26:06 +0100

    Restructure repository

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Andrew Ying 2019.
+ *
+ * This file is part of the Intelligent Platform Management Interface (IPMI) software.
+ * IPMI is licensed under the API Copyleft License. A copy of the license is available
+ * at LICENSE.md.
+ *
+ * As far as the law allows, this software comes as is, without any warranty or
+ * condition, and no contributor will be liable to anyone for any damages related
+ * to this software or this license, under any kind of legal claim.
+ */
+
+import "./css/app";
+
+import "keypress";
+import "./js/App";
---
+++ src/js/components/Images.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Andrew Ying 2019.
+ *
+ * This file is part of the Intelligent Platform Management Interface (IPMI) software.
+ * IPMI is licensed under the API Copyleft License. A copy of the license is available
+ * at LICENSE.md.
+ *
+ * As far as the law allows, this software comes as is, without any warranty or
+ * condition, and no contributor will be liable to anyone for any damages related
+ * to this software or this license, under any kind of legal claim.
+ */
+
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+class Images extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default withRouter(Images);
