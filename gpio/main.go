4ff3228ad8275abb7e7b504199e24f0510aea35c
Andrew Ying <hi@andrewying.com>
2019-04-14 11:54:46 +0100

    Add GPIO handler

---
+++ gpio/main.go
@@ -0,0 +1,84 @@
+/*
+ * Adsisto
+ * Copyright (c) 2019 Andrew Ying
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU General Public License as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package gpio
+
+import (
+	"errors"
+	"periph.io/x/periph/conn/gpio"
+	"periph.io/x/periph/conn/gpio/gpioreg"
+	"periph.io/x/periph/host"
+	"time"
+)
+
+type Config struct {
+	Pins []string
+}
+
+var (
+	ErrPinUndefined = errors.New("GPIO pin not defined in configuration")
+)
+
+func (c *Config) SetupPins() []error {
+	var ers []error
+
+	if _, err := host.Init(); err != nil {
+		ers = append(ers, err)
+		return ers
+	}
+
+	for _, pin := range c.Pins {
+		p := gpioreg.ByName(pin)
+		err := p.Out(gpio.Low)
+		if err != nil {
+			ers = append(ers, err)
+		}
+	}
+	return ers
+}
+
+func (c *Config) TogglePin(pin string) error {
+	found := false
+
+	for _, p := range c.Pins {
+		if pin == p {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return ErrPinUndefined
+	}
+
+	if _, err := host.Init(); err != nil {
+		return err
+	}
+
+	p := gpioreg.ByName(pin)
+	err := p.Out(gpio.High)
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(time.Second * 2)
+	err = p.Out(gpio.Low)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
