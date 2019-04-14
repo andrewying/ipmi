1206484c14ae3be6be6c3a65166638ca9f3dd29b
Andrew Ying <hi@andrewying.com>
2019-04-14 13:51:38 +0100

    Remove files no longer needed

--- public/input.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	system("v4l2-ctl -d ".$_POST['vid']." --set-input=".$_POST['inp']);
-
-?>
--- public/key.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$tty = $_POST['tty'];
-	$key = $_POST['k'];
-	$cmd='echo "echo \''.$key.'\' | /home/pi/sendkeys /dev/hidg0 keyboard" >> '.$tty;
-	system($cmd);
-?>
